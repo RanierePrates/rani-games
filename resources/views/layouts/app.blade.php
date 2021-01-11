dc4a722990d751b0e3d73c8b1f6c525871e41475
Raniere Prates <rsilva@uplexis.com.br>
2021-01-11 17:02:01 -0300

    iniciei projeto

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rani Games</title>
+    <link rel="stylesheet" href="">
+</head>
+<body class="bg-gray-900 text-white">
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 px-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-logo.svg" alt="" class="w-32 flex-none">
+                </a>
+                <ul class="flex">
+                    <li>
+                        <a href="" class="hover:text-gray-400"></a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+</body>
+</html>
